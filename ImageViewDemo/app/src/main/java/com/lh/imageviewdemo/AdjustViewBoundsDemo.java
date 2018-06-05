0293a7e5391bcf196f33e2da5b20c3fde6fb54fd
刘皓 <312761200@qq.com>
2018-06-05 18:21:20 +0800

    0605

---
+++ ImageViewDemo/app/src/main/java/com/lh/imageviewdemo/AdjustViewBoundsDemo.java
@@ -0,0 +1,13 @@
+package com.lh.imageviewdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AdjustViewBoundsDemo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adjust_view_bounds_demo);
+    }
+}
